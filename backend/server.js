f89959d05ab7b8b5c2578dfa2499b6a9b902346d
abhisheksharma <abhisheksharma131098@gmail.com>
2025-05-25 18:47:42 +0530

    create frontend and connect it to backend

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 import userRoute from "./routes/userRoutes.js";
-import connectDB from "./configs/db.confog.js";
+import connectDB from "./configs/db.config.js";
 import { notFound, errorHandler } from "./middlewares/errorMiddleware.js";
 
 dotenv.config();
@@ -13,6 +14,14 @@ connectDB();
 
 const app = express();
 
+// If you need to allow credentials (cookies, auth tokens)
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -20,7 +29,7 @@ app.use(cookieParser());
 
 app.get("/", (req, res) => res.send("Server Started"));
 
-app.use("/user", userRoute);
+app.use("/api/user", userRoute);
 
 app.use(notFound);
 app.use(errorHandler);
